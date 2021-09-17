9f514594429e3247c72af97bf7ecd0d5e17f8f40
Mango LeFroot <dtsmith494@gmail.com>
2021-09-17 02:24:57 +0000

    Added signal handling

--- main.py
+++ main.py
@@ -4,36 +4,44 @@ __author__ = "Mango LeFroot"
 __version__ = "0.1.0"
 __license__ = "MIT"
 
-from PIL import Image
-from PIL import ImageDraw
-import time
+import sys, signal
 from rgbmatrix import RGBMatrix, RGBMatrixOptions
+
+interrupt_received = False
+def signal_handler(signal, frame):
+    global interrupt_received
+    interrupt_received = True
+
 
 OptionsFace = None
 MatrixFace = None
 
 
 def setup():
-    OptionsFace = RGBMatrixOptions()
-    OptionsFace.hardware_mapping = "adafruit-hat-pwm"
-    OptionsFace.cols = 64
-    OptionsFace.rows = 32
-    OptionsFace.chain_length = 2
-    OptionsFace.parallel = 1
+    signal.signal(signal.SIGTERM, signal_handler)
+    signal.signal(signal.SIGINT, signal_handler)
 
-    OptionsFace.brightness = 80
-    OptionsFace.led_rgb_sequence = "BRG"
-    OptionsFace.limit_refresh_rate_hz = 90
+    #OptionsFace = RGBMatrixOptions()
+    #OptionsFace.hardware_mapping = "adafruit-hat-pwm"
+    #OptionsFace.cols = 64
+    #OptionsFace.rows = 32
+    #OptionsFace.chain_length = 2
+    #OptionsFace.parallel = 1
 
-    MatrixFace = RGBMatrix(option = OptionsFace)
+    #OptionsFace.brightness = 80
+    #OptionsFace.led_rgb_sequence = "BRG"
+    #OptionsFace.limit_refresh_rate_hz = 90
+
+    #MatrixFace = RGBMatrix(option = OptionsFace)
 
 def loop():
-    print("hello world")
+    while not interrupt_received:
+        print("Hello World!")
 
 def main():
     setup()
     loop()
-
+    sys.exit(0)
 
 if __name__ == "__main__":
     main()
